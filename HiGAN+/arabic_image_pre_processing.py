4b6aa84e676c89a39aa9ae4310b86e5176a3691d
taskins1 <taskins@ethz.ch>
2023-01-12 10:14:36 +0100

    the detected contour is still random

--- HiGAN+/arabic_image_pre_processing.py
+++ HiGAN+/arabic_image_pre_processing.py
@@ -1,23 +1,9 @@
-# Crop image from center
-# remove whitespace
-# convert to grayscale
-# resize to 64X64
-
 import cv2
 import os
 from os import path
 from PIL import Image
 from PIL import ImageOps
 import imutils
-
-
-def crop_center(pil_img, crop_width, crop_height):
-    img_width, img_height = pil_img.size
-    return pil_img.crop(((img_width - crop_width) // 2,
-                         (img_height - crop_height) // 2,
-                         (img_width + crop_width) // 2,
-                         (img_height + crop_height) // 2))
-
 
 input_dir_name = path.join("data/arabic_isolated_words_per_user")
 
@@ -30,37 +16,55 @@ for folder in os.listdir(input_dir_name):
         print("file_name", file_name)
         image = Image.open(os.path.join(input_dir, file_name))
         image.load()
-        imageSize = image.size
-        print("Showing Original Images:")
         image.show()
 
         # Resizing Image
         string_parts = file_name.split('_')
         word_length = len(string_parts[1])
-        print("word_length", word_length)
         img_width = 16 * word_length
-        resized_image = imutils.resize(image, width=img_width, height=32)
-        # resized_image = crop_center(image, 16*word_length, imageSize[0])
+        print("word_length", word_length)
 
-        # Inversion
-        # invert_im = resized_image.convert("RGB")
-        # print("Showing Inverted Images:")
-        # invert_im.show()
-        # imageBox = invert_im.getbbox()
+        # Get the original width and height
+        imageSize = image.size
+        width, height = image.size
 
-        imageBox = image.getbbox()
-        cropped_image = image.crop(imageBox)
-        print("%s Size:%s New Size:%s" % (file_name, imageSize, imageBox))
-        cropped_image.save(os.path.join(input_dir, file_name))
+        # Calculate the new width and height
+        new_height = 64
+        new_width = 16 * word_length
 
-    # For every file in the input dir and resize to [32 * 16*len(text)]
-    for file in os.listdir(input_dir):
-        original_image = cv2.imread(os.path.join(input_dir, file))
-        cv2.imshow('original', original_image)
-        # resize image with aspect ratio maintained
-        print("file", file)
-        img_width = 16 * len(file)  # TODO: Need the length of the arabic word in the file name
-        resized_img = imutils.resize(original_image, width=img_width, height=32)
-        # resized_img = cv2.resize(original_image, (128, 128), cv2.INTER_AREA)
-        cv2.imshow('resized', resized_img)
-        cv2.imwrite(os.path.join(input_dir, file), resized_img)
+        # Height adjust the image
+        image = image.resize((new_width, new_height), resample=Image.LANCZOS)
+        print("Show 1")
+        image.show()
+
+        # Crop or pad the image
+        if width > new_width:
+            # Crop the image by slicing off the excess on the right
+            image = image.crop((0, 0, new_width, new_height))
+        elif width < new_width:
+            # Pad the image by creating a new image with the desired size and pasting the original image in the center
+            padded_im = Image.new("RGB", (new_width, new_height), (255, 255, 255))
+            padded_im.paste(image, ((new_width - width) // 2, (new_height - height) // 2))
+            image = padded_im
+
+        print("Show 2")
+        image.show()
+
+        # Save the modified image
+        imageNewSize = image.size
+        image.save("image_cropped_or_padded.png")
+        # resized_image.save(os.path.join(input_dir, file_name))
+
+        print("%s Size:%s New Size:%s" % (file_name, imageSize, imageNewSize))
+
+    # # For every file in the input dir and resize to [32 * 16*len(text)]
+    # for file in os.listdir(input_dir):
+    #     original_image = cv2.imread(os.path.join(input_dir, file))
+    #     cv2.imshow('original', original_image)
+    #     # resize image with aspect ratio maintained
+    #     print("file", file)
+    #     img_width = 16 * len(file)  # Need the length of the arabic word in the file name
+    #     resized_img = imutils.resize(original_image, width=img_width, height=32)
+    #     # resized_img = cv2.resize(original_image, (128, 128), cv2.INTER_AREA)
+    #     cv2.imshow('resized', resized_img)
+    #     cv2.imwrite(os.path.join(input_dir, file), resized_img)
---
+++ HiGAN+/arabic_image_pre_processing_2.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import os
+from os import path
+from PIL import Image
+
+input_dir_name = path.join("data/arabic_isolated_words_per_user")
+
+for folder in os.listdir(input_dir_name):
+    print(folder)
+    input_dir = os.path.join(input_dir_name, folder)
+    assert path.exists(input_dir)
+
+    for file_name in os.listdir(input_dir):
+        print("file_name", file_name)
+        # image = Image.open(os.path.join(input_dir, file_name))
+        image = cv2.imread(os.path.join(input_dir, file_name))
+
+        # Desired Size of Image
+        string_parts = file_name.split('_')
+        word_length = len(string_parts[1])
+        img_width = 32 * word_length
+        print("word_length", word_length)
+
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Denoiseing Image
+        # Apply erosion to the binary image
+        # denoised = cv2.GaussianBlur(gray, (5, 5), 0)
+        denoised = cv2.erode(gray, (5, 5), iterations=1)
+        print("Show image after denoising:")
+        cv2.imshow("denoised", denoised)  # Display the image
+        cv2.waitKey(0)  # Wait for a key press
+        cv2.destroyAllWindows()  # Close the window
+
+        # Threshold the image to binary
+        _, binary = cv2.threshold(denoised, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        print("Show image after binarification:")
+        cv2.imshow("binary", binary)  # Display the image
+        cv2.waitKey(0)  # Wait for a key press
+        cv2.destroyAllWindows()  # Close the window
+
+        # Find the contours of the handwriting
+        contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Find the bounding box of the handwriting
+        x, y, w, h = cv2.boundingRect(contours[0])
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        print("drawing box on image")
+        cv2.imshow("image", image)  # Display the image
+        cv2.waitKey(0)  # Wait for a key press
+        cv2.destroyAllWindows()  # Close the window
+
+        # Crop the image to the bounding box
+        handwriting = gray[y:y + h, x:x + w]
+        print("Show image after cropping:")
+        cv2.imshow("handwriting", handwriting)  # Display the image
+        cv2.waitKey(0)  # Wait for a key press
+        cv2.destroyAllWindows()  # Close the window
+
+        # Or cut away the white pixels if shape not the same
+        target_shape = (64, img_width)
+        if handwriting.shape[1] < target_shape[1]:
+            print("handwriting.shape[1] < target_shape[1]")
+        if handwriting.shape[1] != target_shape[1]:
+            handwriting = handwriting[:, :target_shape[1]]
+
+        # Scale the image
+        handwriting = cv2.resize(handwriting, target_shape, interpolation=cv2.INTER_AREA)
+        print("Show after scaling")
+        cv2.imshow("Handwriting", handwriting)  # Display the image
+        cv2.waitKey(0)  # Wait for a key press
+        cv2.destroyAllWindows()  # Close the window
+
+        # padding the image if width not enough long
+        if handwriting.shape[1] < target_shape[1]:
+            padding_width = (0, target_shape[1] - handwriting.shape[1])
+            handwriting = np.pad(handwriting, padding_width, mode='constant', constant_values=255)
+
+        print("------next_image-------")
