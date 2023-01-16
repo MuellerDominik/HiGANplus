b900c32fd1f8dd0e9c6926a7e39c2c66a91d6a06
taskins1 <taskins@ethz.ch>
2023-01-16 10:24:37 +0100

    Ready to submit V2

--- HiGAN+/arabic_image_pre_processing.py
+++ HiGAN+/arabic_image_pre_processing.py
@@ -5,9 +5,15 @@ from os import path
 from PIL import Image
 from arabic_hdf5_dataset_generation import gen_h5file
 
+# Choose whether the train or test set needs to be generated:
+is_test = True
+# is_test = False
+
 # Choosing Input Directory
-input_dir_name = path.join("data/arabic_isolated_words_per_user_test")
-# input_dir_name = path.join("data/arabic_isolated_words_per_user_new_train")
+if is_test == True:
+    input_dir_name = path.join("data/arabic_isolated_words_per_user_test")
+else:
+    input_dir_name = path.join("data/arabic_isolated_words_per_user_train")
 
 all_wids = np.empty((0,))
 all_texts = []
@@ -24,11 +30,10 @@ for folder in os.listdir(input_dir_name):
         # image = Image.open(os.path.join(input_dir, file_name))
         image = cv2.imread(os.path.join(input_dir, file_name))
 
-        # Desired Size of Image
+        # Image Information:
         string_parts = file_name.split('_')
         # print("string_parts", string_parts)
         word_length = len(string_parts[1])
-        img_width = 32 * word_length
         # print("word_length", word_length)
 
         # Saving the words to generate a txt file
@@ -51,59 +56,54 @@ for folder in os.listdir(input_dir_name):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # Denoiseing Image
-        # Apply erosion to the binary image
-        # denoised = cv2.GaussianBlur(gray, (5, 5), 0)
+        #  Apply erosion to the binary image
         denoised = cv2.erode(gray, (5, 5), iterations=1)
-        # print("Image after denoising:")
-        # cv2.imshow("denoised", denoised)  # Display the image
-        # cv2.waitKey(0)  # Wait for a key press
-        # cv2.destroyAllWindows()  # Close the window
+        # To check the current state of image:
+        #  cv2.imshow("denoised", denoised)  # Display the image
+        #  cv2.waitKey(0)  # Wait for a key press
+        #  cv2.destroyAllWindows()  # Close the window
 
         # Threshold the image to binary
         _, binary = cv2.threshold(denoised, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        # print("Image after binarification:")
-        # cv2.imshow("binary", binary)  # Display the image
-        # cv2.waitKey(0)  # Wait for a key press
-        # cv2.destroyAllWindows()  # Close the window
+        # To check the current state of image:
+        #  cv2.imshow("binary", binary)  # Display the image
+        #  cv2.waitKey(0)  # Wait for a key press
+        #  cv2.destroyAllWindows()  # Close the window
 
         # Find the contours of the handwriting
         # 1.Try Contours: contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         x, y, w, h = cv2.boundingRect(binary)
         cv2.rectangle(binary, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        # print("Image with box")
-        # cv2.imshow("binary with box", binary)  # Display the image
-        # cv2.waitKey(0)  # Wait for a key press
-        # cv2.destroyAllWindows()  # Close the window
+        # To check the current state of image:
+        #  cv2.imshow("binary with box", binary)  # Display the image
+        #  cv2.waitKey(0)  # Wait for a key press
+        #  cv2.destroyAllWindows()  # Close the window
 
         # Crop the image to the bounding box
         handwriting = gray[y:y + h, x:x + w]
-        # print("Image after cropping:")
-        # cv2.imshow("gray image cropped", handwriting)  # Display the image
-        # cv2.waitKey(0)  # Wait for a key press
-        # cv2.destroyAllWindows()  # Close the window
+        # To check the current state of image:
+        #  cv2.imshow("gray image cropped", handwriting)  # Display the image
+        #  cv2.waitKey(0)  # Wait for a key press
+        #  cv2.destroyAllWindows()  # Close the window
 
         # Resize the images while keeping the aspect ratio the same
-        # print("handwriting.shape before resizing", handwriting.shape)
-        target_shape = (64, img_width)
-        height, width = handwriting.shape[:2]
         original_aspect_ratio = handwriting.shape[1] / handwriting.shape[0]
         # print("shape:", handwriting.shape[1], handwriting.shape[0])
 
         resized_image = cv2.resize(handwriting, (int(64 * original_aspect_ratio), 64))
-        # print("handwriting.shape after resizing", resized_image.shape)
-        # cv2.imshow("Resized handwriting", resized_image)  # Display the image
-        # cv2.waitKey(0)  # Wait for a key press
-        # cv2.destroyAllWindows()  # Close the window
+        # To check the current state of image:
+        #  cv2.imshow("Resized handwriting", resized_image)  # Display the image
+        #  cv2.waitKey(0)  # Wait for a key press
+        #  cv2.destroyAllWindows()  # Close the window
 
         # Inverting the image:
         _, resized_binary = cv2.threshold(resized_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        # cv2.imshow("Resized binary handwriting", resized_binary)  # Display the image
-        # cv2.waitKey(0)  # Wait for a key press
-        # cv2.destroyAllWindows()  # Close the window
+        # To check the current state of image:
+        #  cv2.imshow("Resized binary handwriting", resized_binary)  # Display the image
+        #  cv2.waitKey(0)  # Wait for a key press
+        #  cv2.destroyAllWindows()  # Close the window
 
         # Numpy matrix with all the pre-processed images:
-        # vectorized_image = resized_binary.reshape(1, -1)
-        # print("vectorized_image.shape", vectorized_image.shape)
         all_imgs.append(resized_binary)
         # print(len(all_imgs))
 
@@ -111,15 +111,18 @@ for folder in os.listdir(input_dir_name):
 
 print("end_of_dataset")
 
-# Hdf5 File Generation Function.
-#  Choose whether to generate the train or test hfd5 file here. Note that the input directory above needs
-#  to be set accordingly.
-gen_h5file(all_imgs, all_texts, all_wids, 'arabic_hdf5_file_train')
-# gen_h5file(all_imgs, all_texts, all_wids, 'arabic_hdf5_file_test')
+# Hdf5 File Generation:
+if is_test == True:
+    gen_h5file(all_imgs, all_texts, all_wids, 'arabic_hdf5_file_test')
+else:
+    gen_h5file(all_imgs, all_texts, all_wids, 'arabic_hdf5_file_train')
 
 
-# Open a file for writing
-with open('arabic_test_words.txt', 'w') as f:
-    # Write each item in the list to the file on a new line
-    for item in words_list:
-        f.write(str(item) + '\n')
+# Generating a txt file to test the model: c
+if is_test == True:
+    with open('arabic_test_words.txt', 'w') as f:
+        # Write each item in the list to the file on a new line
+        for item in words_list:
+            f.write(str(item) + '\n')
+else:
+    pass
--- README.md
+++ README.md
@@ -1,94 +1,29 @@
-# HiGAN+
 
-## Introduction
-This is a PyTorch implementation of the paper **"HiGAN+: Handwriting Imitation GAN with Disentangled
-Representations"** (authored by **Ji Gan, Weiwiang Wang\*, Jiaxu Leng, Xinbo Gao\*.** )
+# Handwritten Text Generation using cGAN: Sub-experiment 'Arabic Handwriting Generation'
 
-HiGAN+ can generate diverse and realistic handwritten text images (with **64**-pixel height) conditioned
-on arbitrary textual contents and calligraphic styles.
+This experiment aims to answer whether the HiGAN+ can be generalized to the Arabic language.
 
-## Overview of HiGAN+
-![Overview of HiGAN](docs/imgs/Overview.png)
+## Requirements
 
+Install the packages listed in the requirement.txt file.
 
-## Installation & requirements
-The current version of the code has been tested with the following environment:
-- Ubuntu 20 or 22
-- Python 3
-- PyTorch 1.11.0
+## Recreation of results
 
+To run the model, first the arabic handwriting dataset AHAWP needs to be downloaded. In the dataset the sub-folder
+'isolated_words_per_user'. The folder needs to be put into the sub-folder 'data' and split to 
+'arabic_isolated_words_per_user_train' and 'arabic_isolated_words_per_user_test'. In our case we use 18 users 
+in the test set and the rest of the users in the train set. 
 
-To use the code, download the repository and change into it:
+Afterwards the python file named 'arabic_image_pre_processing' needs to be run. This file does get the labels,
+the writer ID, crops the image according to the original IAM datset and inverts it. At the end
+the 'arabic_hdf5_dataset_generation.py' is executed which generates the needed .hdf5 files to run the model. 
 
-`git clone https://github.com/ganji15/HiGANplus.git`
+In order to run the code without adjusting the source code otherwise, the generated test and train sets need to be 
+renamed to 'trnvalset_words64_OrgSz.hdf5' and 'testset_words64_OrgSz.hdf5' respectively. 
 
-`cd HiGAN+`
+Furthermore a txt file named 'arabic_test_words.txt' is generated when running 
 
-You need to applicant the IAM dataset from <http://www.fki.inf.unibe.ch/databases/iam-handwriting-database> and then extract the handwriting images. 
-For convenience, here we provide the processed h5py files [trnvalset_words64_OrgSz.hdf5](https://github.com/ganji15/HiGANplus/releases/download/dataset/trnvalset_words64_OrgSz.hdf5)  [testset_words64_OrgSz.hdf5](https://github.com/ganji15/HiGANplus/releases/download/dataset/testset_words64_OrgSz.hdf5), which should put into the **./data/iam/** directory.
+The 'reading_hdf5_files.py' was used to reverse engineer the dataset structure of the source code and for debugging
+purposes only. 
 
-
-## Training & Test
-### Training HiGAN on the IAM dataset
-`python train.py --config ./configs/gan_iam.yml`
-
-### Quantitative Test
-`python test.py --config ./configs/gan_iam.yml --ckpt ./pretrained/deploy_HiGAN+.pth --guided True`
-+ Main arguments:
-  - `--config`: the configuration file of HiGAN
-  - `--ckpt`: the path of checkpoint, which is stored in the `./runs/` directory after training.
-  - `--guided`: whether to extract styles from reference images. If `--guided False`, the styles of generated images will be randomly sampled from the standard normal distribution.
-
-### Qualitative Evaluation
-`python eval_demo.py --config ./configs/gan_iam.yml --ckpt ./pretrained/deploy_HiGAN+.pth --mode style`
-+ Main arguments:
-  - `--config`: the configuration file of HiGAN
-  - `--ckpt`: the path of checkpoint, which is stored in the `./runs/` directory after training.
-  - `--mode`: `[ rand | style | interp | text ]`.
-
-#### Latent-guided synthesis
-`python eval_demo.py --config ./configs/gan_iam.yml --ckpt ./pretrained/deploy_HiGAN+.pth --mode rand`
-![Rand](docs/imgs/GenRand.png)
-
-#### Reference-guided synthesis
-`python eval_demo.py --config ./configs/gan_iam.yml --ckpt ./pretrained/deploy_HiGAN+.pth --mode style`
-![Style](docs/imgs/GenStyle.png)
-
-#### Text synthesis
-`python eval_demo.py --config ./configs/gan_iam.yml --ckpt ./pretrained/deploy_HiGAN+.pth --mode text`
-![Text](docs/imgs/GenText.png)
-
-#### Style interpolation
-`python eval_demo.py --config ./configs/gan_iam.yml --ckpt ./pretrained/deploy_HiGAN+.pth --mode interp`
-![Interp1](docs/imgs/GenInterp.png)
-
-
-## On-the-fly plots during training
-With this code it is possible to track progress during training with on-the-fly plots. This feature requires `Tensorboard`, which should be started from the command line:
-
-`tensorboard --logdir=./runs`
-
-The tensorboard server is now alive and can be accessed at http://localhost:6006.
-
-Some on-the-fly plots are given as the followings:
-![Loss](docs/imgs/tensorboard_log.png)
-![Samples](docs/imgs/tensorboard_vis.png)
-
-
-## Citation
-If you find our research is helpful, please remember to cite our paper:
-```
-@article{gan2022higanplus,
-author = {Gan, Ji and Wang, Weiqiang and Leng, Jiaxu and Gao, Xinbo},
-title = {HiGAN+: Handwriting Imitation GAN with Disentangled Representations},
-year = {2022},
-volume = {42},
-number = {1},
-url = {https://doi.org/10.1145/3550070},
-doi = {10.1145/3550070},
-journal = {ACM Trans. Graph.}
-}
-```
-
-## License
-HiGAN+ is free for academic research purposes.
+The 'README_of_HiGAN+_Source_Code.md' file is the mardown README file of the source code. 
